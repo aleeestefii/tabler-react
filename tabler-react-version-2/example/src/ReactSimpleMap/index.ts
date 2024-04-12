17d75130dd4179d97a92af3cfc8cab67393a0b5a
Alejnadra Estefanía Rico González <A01749850@tec.mx>
2024-04-12 10:46:55 -0600

    bye

--- tabler-react-version-2/example/src/ReactSimpleMap/index.ts
+++
@@ -1,3 +0,0 @@
-import ReactSimpleMap from "./ReactSimpleMap";
-
-export { ReactSimpleMap as default };
--- tabler-react-version-2/example/src/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "react-c3js";
-declare module "d3-scale";
-declare module "react-simple-maps";
