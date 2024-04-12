5a0d195c576c88a12b87605656888469b1cfe800
Alejnadra Estefanía Rico González <A01749850@tec.mx>
2024-04-12 09:50:40 -0600

    Official document
    How it would graphically look

---
+++ tabler_react/example/src/pages/500.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { Error500Page } from "tabler-react";
+
+type Props = {};
+
+function Error500(props: Props) {
+  return <Error500Page />;
+}
+
+export default Error500;
